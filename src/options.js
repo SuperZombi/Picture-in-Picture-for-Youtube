0ede4b973c3a55403b00b235d317b36c37bcf656
Super Zombi <rost201541@gmail.com>
2021-10-06 00:37:22 +0300

    Update options.js

--- src/options.js
+++ src/options.js
@@ -1,10 +1,33 @@
-chrome.storage.sync.get({ changeIcon: true }, results => {
+chrome.storage.sync.get({ changeIcon: true, animation: "default" }, results => {
   const changeIconCheckbox = document.querySelector('#changeIcon');
+  
+  function ChangeColor() {
+    var clrDiv = document.getElementById("animation")
+    if (!changeIconCheckbox.checked){
+      clrDiv.style.backgroundColor = "#E7E7E7";
+      clrDiv.style.filter = "blur(1px)";
+      clrDiv.style.pointerEvents = "none";
+      clrDiv.style.userSelect = "none";
+    }
+    else{
+      clrDiv.style.backgroundColor = "";
+      clrDiv.style.filter = "";
+      clrDiv.style.pointerEvents = "auto";
+      clrDiv.style.userSelect = "auto";
+    }
+  }
+  changeIconCheckbox.onclick = function() { ChangeColor(); }
 
   changeIconCheckbox.checked = results.changeIcon;
-  changeIconCheckbox.onchange = _ => {
+  ChangeColor()
+
+  document.querySelector(`input[value="${results.animation}"]`).checked = "checked"
+
+
+  document.getElementById("save").onclick = _ => {
     chrome.storage.sync.set({
-      changeIcon: changeIconCheckbox.checked
+      changeIcon: changeIconCheckbox.checked,
+      animation: document.querySelector('input[name="animation"]:checked').value
     }, _ => {
       // Reload extension to make opt-out change immediate. 
       chrome.runtime.reload();

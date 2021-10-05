d783f51a2ec09aff42f1b0aedc3d370bf1eefb19
Super Zombi <rost201541@gmail.com>
2021-10-06 00:36:49 +0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
-chrome.storage.sync.get({ changeIcon: true }, results => {
+chrome.storage.sync.get({ changeIcon: true, animation: "default" }, results => {
 	change = results.changeIcon;
+	animation = results.animation;
 });
 var oldHref = document.location.href;
 
@@ -36,35 +37,53 @@ function main(){
 		});
 
 		if (change){
-			document.addEventListener('enterpictureinpicture', () => {
-				g = document.getElementById('pip_svg');
-				g.innerHTML = "";
+			if (animation == "animation_1"){
+				document.addEventListener('enterpictureinpicture', () => {
+					document.getElementById('pip_custom_path').style.transform = "translate(2450px, 2100px) rotateY(180deg) rotateX(180deg)"
+				});
+				document.addEventListener('leavepictureinpicture', () => {
+					document.getElementById('pip_custom_path').style.transform = ""
+				});
+			}
+			else if (animation == "animation_2"){
+				document.addEventListener('enterpictureinpicture', () => {
+					document.getElementById('pip_custom_path').style.transform = "translateY(2100px) translateX(2450px) rotate(-180deg)"
+				});
+				document.addEventListener('leavepictureinpicture', () => {
+					document.getElementById('pip_custom_path').style.transform = ""
+				});
+			}
+			else{
+				document.addEventListener('enterpictureinpicture', () => {
+					g = document.getElementById('pip_svg');
+					g.innerHTML = "";
 
-				path1 = document.createElement('path');
-				path1.setAttribute('d', "M1645 1619 c-11 -6 -172 -163 -357 -348 l-338 -336 0 142 c0 137 -1 142 -26 172 -32 38 -80 49 -122 27 -17 -9 -36 -25 -42 -35 -14 -26 -14 -586 0 -612 24 -45 53 -49 333 -49 167 0 276 4 296 11 64 22 80 107 32 161 l-29 33 -148 5 -147 5 244 238 c134 131 290 287 347 347 95 102 102 113 102 151 0 75 -79 123 -145 88z");
-				path2 = document.createElement('path');
-				path2.setAttribute('d', "M26 1584 l-26 -27 0 -664 0 -664 25 -24 24 -25 836 0 837 0 29 29 29 29 0 307 c0 325 -3 350 -49 375 -35 18 -87 12 -120 -16 l-31 -26 0 -249 0 -249 -690 0 -690 0 0 515 0 515 411 0 411 0 29 29 c41 41 41 101 0 142 l-29 29 -485 0 -484 0 -27 -26z");
-				
-				g.appendChild(path1);
-				g.appendChild(path2);
+					path1 = document.createElement('path');
+					path1.setAttribute('d', "M1645 1619 c-11 -6 -172 -163 -357 -348 l-338 -336 0 142 c0 137 -1 142 -26 172 -32 38 -80 49 -122 27 -17 -9 -36 -25 -42 -35 -14 -26 -14 -586 0 -612 24 -45 53 -49 333 -49 167 0 276 4 296 11 64 22 80 107 32 161 l-29 33 -148 5 -147 5 244 238 c134 131 290 287 347 347 95 102 102 113 102 151 0 75 -79 123 -145 88z");
+					path2 = document.createElement('path');
+					path2.setAttribute('d', "M26 1584 l-26 -27 0 -664 0 -664 25 -24 24 -25 836 0 837 0 29 29 29 29 0 307 c0 325 -3 350 -49 375 -35 18 -87 12 -120 -16 l-31 -26 0 -249 0 -249 -690 0 -690 0 0 515 0 515 411 0 411 0 29 29 c41 41 41 101 0 142 l-29 29 -485 0 -484 0 -27 -26z");
+					
+					g.appendChild(path1);
+					g.appendChild(path2);
 
-				g.innerHTML += "";
-			});
-			document.addEventListener('leavepictureinpicture', () => {
-				g = document.getElementById('pip_svg');
-				g.innerHTML = "";
+					g.innerHTML += "";
+				});
+				document.addEventListener('leavepictureinpicture', () => {
+					g = document.getElementById('pip_svg');
+					g.innerHTML = "";
 
-				path1 = document.createElement('path');
-				path1.setAttribute('d', "M1348 1825 c-35 -19 -48 -44 -48 -90 0 -46 23 -81 62 -94 17 -6 88 -11 157 -11 l125 0 -342 -343 -342 -343 0 -51 c0 -45 4 -54 31 -77 22 -19 42 -26 72 -26 41 0 44 3 389 347 l348 348 0 -138 c0 -155 9 -186 59 -207 42 -17 95 -5 121 27 19 25 20 39 20 319 l0 293 -24 28 -24 28 -289 2 c-241 2 -292 0 -315 -12z");
-				path2 = document.createElement('path');
-				path2.setAttribute('d', "M26 1584 l-26 -27 0 -664 0 -664 25 -24 24 -25 836 0 837 0 29 29 29 29 0 307 c0 325 -3 350 -49 375 -35 18 -87 12 -120 -16 l-31 -26 0 -249 0 -249 -690 0 -690 0 0 515 0 515 411 0 411 0 29 29 c41 41 41 101 0 142 l-29 29 -485 0 -484 0 -27 -26z");
-				
-				g.appendChild(path1);
-				g.appendChild(path2);
+					path1 = document.createElement('path');
+					path1.setAttribute('d', "M1348 1825 c-35 -19 -48 -44 -48 -90 0 -46 23 -81 62 -94 17 -6 88 -11 157 -11 l125 0 -342 -343 -342 -343 0 -51 c0 -45 4 -54 31 -77 22 -19 42 -26 72 -26 41 0 44 3 389 347 l348 348 0 -138 c0 -155 9 -186 59 -207 42 -17 95 -5 121 27 19 25 20 39 20 319 l0 293 -24 28 -24 28 -289 2 c-241 2 -292 0 -315 -12z");
+					path2 = document.createElement('path');
+					path2.setAttribute('d', "M26 1584 l-26 -27 0 -664 0 -664 25 -24 24 -25 836 0 837 0 29 29 29 29 0 307 c0 325 -3 350 -49 375 -35 18 -87 12 -120 -16 l-31 -26 0 -249 0 -249 -690 0 -690 0 0 515 0 515 411 0 411 0 29 29 c41 41 41 101 0 142 l-29 29 -485 0 -484 0 -27 -26z");
+					
+					g.appendChild(path1);
+					g.appendChild(path2);
 
-				g.innerHTML += "";
-			});
-
+					g.innerHTML += "";
+				});
+			}
+			
 
 			button.style.transform = 'scale(0.9)';
 
@@ -83,6 +102,10 @@ function main(){
 			g.setAttribute('id', 'pip_svg');
 
 			path1 = document.createElement('path');
+			path1.id = "pip_custom_path"
+			path1.style.transformOrigin = 'center'
+			path1.style.transition = "0.25s cubic-bezier(0, 0.5, 0.2, 1)"
+			
 			path1.setAttribute('d', "M1348 1825 c-35 -19 -48 -44 -48 -90 0 -46 23 -81 62 -94 17 -6 88 -11 157 -11 l125 0 -342 -343 -342 -343 0 -51 c0 -45 4 -54 31 -77 22 -19 42 -26 72 -26 41 0 44 3 389 347 l348 348 0 -138 c0 -155 9 -186 59 -207 42 -17 95 -5 121 27 19 25 20 39 20 319 l0 293 -24 28 -24 28 -289 2 c-241 2 -292 0 -315 -12z");
 			path2 = document.createElement('path');
 			path2.setAttribute('d', "M26 1584 l-26 -27 0 -664 0 -664 25 -24 24 -25 836 0 837 0 29 29 29 29 0 307 c0 325 -3 350 -49 375 -35 18 -87 12 -120 -16 l-31 -26 0 -249 0 -249 -690 0 -690 0 0 515 0 515 411 0 411 0 29 29 c41 41 41 101 0 142 l-29 29 -485 0 -484 0 -27 -26z");

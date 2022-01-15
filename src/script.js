b598b2122549eabe87322df767331e7edf8b1973
Super Zombi <rost201541@gmail.com>
2022-01-15 19:43:34 +0200

    Update script.js

--- src/script.js
+++ src/script.js
@@ -24,6 +24,7 @@ window.onload = function() {
 
 
 const target_svg = "M8,7c0,0.55-0.45,1-1,1S6,7.55,6,7c0-0.55,0.45-1,1-1S8,6.45,8,7z M7,16c-0.55,0-1,0.45-1,1c0,0.55,0.45,1,1,1s1-0.45,1-1 C8,16.45,7.55,16,7,16z M10.79,8.23L21,18.44V20h-3.27l-5.76-5.76l-1.27,1.27C10.89,15.97,11,16.47,11,17c0,2.21-1.79,4-4,4 c-2.21,0-4-1.79-4-4c0-2.21,1.79-4,4-4c0.42,0,0.81,0.08,1.19,0.2l1.37-1.37l-1.11-1.11C8,10.89,7.51,11,7,11c-2.21,0-4-1.79-4-4 c0-2.21,1.79-4,4-4c2.21,0,4,1.79,4,4C11,7.43,10.91,7.84,10.79,8.23z M10.08,8.94L9.65,8.5l0.19-0.58C9.95,7.58,10,7.28,10,7 c0-1.65-1.35-3-3-3S4,5.35,4,7c0,1.65,1.35,3,3,3c0.36,0,0.73-0.07,1.09-0.21L8.7,9.55l0.46,0.46l1.11,1.11l0.71,0.71l-0.71,0.71 L8.9,13.91l-0.43,0.43l-0.58-0.18C7.55,14.05,7.27,14,7,14c-1.65,0-3,1.35-3,3c0,1.65,1.35,3,3,3s3-1.35,3-3 c0-0.38-0.07-0.75-0.22-1.12l-0.25-0.61L10,14.8l1.27-1.27l0.71-0.71l0.71,0.71L18.15,19H20v-0.15L10.08,8.94z M17.73,4H21v1.56 l-5.52,5.52l-2.41-2.41L17.73,4z M18.15,5l-3.67,3.67l1,1L20,5.15V5H18.15z"
+const share_but_svg = "M15,5.63L20.66,12L15,18.37V15v-1h-1c-3.96,0-7.14,1-9.75,3.09c1.84-4.07,5.11-6.4,9.89-7.1L15,9.86V9V5.63 M14,3v6 C6.22,10.13,3.11,15.33,2,21c2.78-3.97,6.44-6,12-6v6l8-9L14,3L14,3z"
 
 function hide_clip_but(){
 	let timerId = setInterval(() => {
@@ -31,6 +32,10 @@ function hide_clip_but(){
 			if (!hided){
 				arr = document.querySelectorAll('#top-level-buttons-computed > ytd-button-renderer')
 				for (let i = 0; i < arr.length; i++){
+					if (share_but_svg == arr[i].getElementsByTagName('svg')[0].getElementsByTagName('path')[0].getAttribute('d')){
+						// share button founded
+						count--;
+					}
 					if (target_svg == arr[i].getElementsByTagName('svg')[0].getElementsByTagName('path')[0].getAttribute('d')){
 						arr[i].remove()
 						hided = true;
@@ -41,13 +46,11 @@ function hide_clip_but(){
 			else{
 				clearInterval(timerId)
 			}
-			count--;
 		}
 		else{
 			clearInterval(timerId)
 		}
 	}, 1000);
-	
 }
 
 
@@ -62,7 +65,7 @@ function main(){
 	if (array.length > 0){
 		if (hideClip){
 			if (youtube_parser(window.location.href)){
-				count = 10;
+				count = 5;
 				hided = false;
 				hide_clip_but()
 			}
